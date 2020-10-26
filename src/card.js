f8f587a3bdb82403de9bb348cc65b61d39dc23b2
bennoureddinezouhour <bennoureddinezouhour970@gmail.com>
2020-10-26 11:47:15 +0100

    'firstcommit'

--- src/card.js
+++ src/card.js
@@ -5,7 +5,7 @@ import{Link} from'react-router-dom';
 const Card=(props)=>{
     return(
     <div className='moviecard'>
-        <Link to="/movie" style={{textDecoration:'none'}}>
+        <Link to={`/movie/${props.element.title}`} style={{textDecoration:'none'}}>
         <img src={props.element.imgurl}  className='img'></img>
     <p style={{textAlign:"center" , fontSize:28,color:'white',margin:20,textDecoration:'none'}} >{props.element.title}</p>
     <p className='des' >{props.element.description}</p>
--- src/description.js
+++ src/description.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import{link} from'react-router-dom';
-import { useHistory } from "react-router-dom"
-const Description=(props)=>{
-    const history = useHistory();
-    const goHome = () => { 
-      history.push("/");
-    };
-  
+import React, { useEffect, useState } from "react";
+import { link } from "react-router-dom";
+ import { useHistory } from "react-router-dom";
+const Description = (props) => {
+  const [movie, setMovie] = useState(null);
+  const history = useHistory();
+  const goHome = () => {
+     history.push("/");
+  };
+  useEffect(() => {
+    // setTimeout(()=> {
+      setMovie(props.list.filter((el) => el.title == props.match.params.name)[0]);
+    // }, 1000)
     
-    return (
-        <div className='centrer'>
-        <h1 >Description : </h1>
-       <p style={{fontSize:18}} >{props.des}</p>
-       <h1>Trailer link : </h1>
-       <p style={{fontSize:18}} >{props.url}</p>
-       <button className="btn btn-secondary mt-4" onClick={goHome}>Logout</button>
-       </div>
-    )
-}
-export default Description
+    console.log(props)
+    // console.log(props.list.filter((el) => el.title == history.location.pathname.replace("/movie/" , ""))[0])
+  });
+
+  return (
+    <div className="centrer">
+      {movie && (
+        <div>
+          <h1>Description : </h1>
+          <p style={{ fontSize: 18 }}>{movie.description}</p>
+          <h1>Trailer link : </h1>
+          <p style={{ fontSize: 18 }}>{movie.posterUrl}</p>
+        </div>
+      )}
+
+      <button className="btn btn-secondary mt-4" onClick={goHome}>
+        Logout
+      </button>
+    </div>
+  );
+};
+export default Description;
